55cb3ea109bd89d15c6ca3d6c7a965e6427ed53f
Anderson Chen <savemuse@gmail.com>
2017-08-31 16:19:26 +0800

    add devtool

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
 import rootSaga from './rootSaga';
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    rootReducer,
-    applyMiddleware(sagaMiddleware)
-  );
+
+  let finalCreateStore = compose(
+    applyMiddleware(sagaMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+    )(createStore)(rootReducer);
 
   sagaMiddleware.run(rootSaga);
-  return store;
+  return finalCreateStore;
 }
